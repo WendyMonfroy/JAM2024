254d07935674aff647802287f9be442dc2080c77
Hugo BARRANDON <hugo.barrandon@ingerop.com>
2024-01-28 14:34:22 +0100

    Add cooldown

--- Assets/Scripts/Wallbreaker.cs
+++ Assets/Scripts/Wallbreaker.cs
@@ -7,13 +7,13 @@ using Screen = UnityEngine.Device.Screen;
 public class Wallbreaker : MonoBehaviour
 {
     public LayerMask IgnoreMe;
+    private int cooldown = 8;
+    private bool canSBreak = true;
     private void Update()
     {
         Ray ray = new Ray(transform.position, transform.forward);
-        //Debug.Log(transform.forward);
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canSBreak)
         {
-            
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 100, ~IgnoreMe)) {
                 GameObject objectHit = hit.transform.gameObject;
@@ -21,8 +21,16 @@ public class Wallbreaker : MonoBehaviour
                 if (objectHit.CompareTag("Wall"))
                 {
                     objectHit.GetComponent<Wall>().DestroyWall();
+                    StartCoroutine(CountDownCoolDown());
                 }
             }
         }
+    }
+
+    private IEnumerator CountDownCoolDown()
+    {
+        canSBreak = false;
+        yield return new WaitForSeconds(cooldown);
+        canSBreak = true;
     }
 }
